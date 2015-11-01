52f65b0daa1b87963c5f0b101761b1eb5423aeb3
joshuagame <joshuagame@gmail.com>
2015-11-01 13:50:03 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+echo "###########################################"
+rm -rf build
+mkdir build
+cd build
+
+cmake ..
+
+make
+
+cd ..
+rm -rf dist
+mkdir dist
+cp build/askeeper* dist
+cp /usr/local/bin/cygmicrohttpd-10.dll dist
